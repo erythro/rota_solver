6aeef21d4fe2eb2b8de287242583eb6510005583
SammySalter <sammy.salter@gmail.com>
2024-10-29 18:46:22 +0000

    forgot to commit this

--- Migrations/1729445672_createSlotTable.py
+++ Migrations/1729445672_createSlotTable.py
@@ -1,2 +1,2 @@
 def run(cursor):
-   cursor.execute('CREATE TABLE event_role(id INTEGER PRIMARY KEY AUTOINCREMENT, event_id INTEGER, role_id INTEGER)')
+   cursor.execute('CREATE TABLE slot(id INTEGER PRIMARY KEY AUTOINCREMENT, event_id INTEGER, role_id INTEGER)')
