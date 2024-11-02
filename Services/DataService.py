36b57de1cfa4db43c0eb889fab36e6192513b4bd
SammySalter <sammy.salter@gmail.com>
2024-11-02 14:03:20 +0000

    changing data service to use a query function

--- Services/DataService.py
+++ Services/DataService.py
@@ -31,24 +31,25 @@ class DataService:
         self.connection = connection
         self.cursor = connection.cursor()
 
+    def query(self, query: str):
+        return self.cursor.execute(query).fetchall()
+        
+
     def getRota(self) -> Rota:
         rota = Rota()
 
         #Load all events (e.g. services)
-        result = self.cursor.execute('SELECT * FROM event')
-        for [id, name] in result.fetchall():
+        for [id, name] in self.query('SELECT * FROM event'):
             rota.events[id] = Event(id, name, dict())
 
         #Load all slots for timeSlots
-        result = self.cursor.execute('SELECT * FROM slot LEFT JOIN role ON slot.role_id = role.id')
-        for [id, event_id, role_id, _, role_name] in result.fetchall():
+        for [id, event_id, role_id, _, role_name] in self.query('SELECT * FROM slot LEFT JOIN role ON slot.role_id = role.id'):
             rota.events[event_id].slots[id] = Slot(id, event_id, role_id, role_name)
         return rota
 
     def getRoles(self) -> dict:
         roles = dict()
-        result = self.cursor.execute('SELECT * FROM person_role LEFT JOIN role ON person_role.role_id = role.id')
-        for [person_id, role_id, _, _, role_name] in result.fetchall():
+        for [person_id, role_id, _, _, role_name] in self.query('SELECT * FROM person_role LEFT JOIN role ON person_role.role_id = role.id'):
             if role_id not in roles:
                 roles[role_id] = Role(role_id, role_name, [])
             roles[role_id].person_ids.append(person_id)
@@ -56,29 +57,25 @@ class DataService:
         
     def rolesPerPersonCounts(self) -> dict:
         roleCounts = dict()
-        result = self.cursor.execute('SELECT person.id, COUNT(person_role.role_id) FROM person LEFT JOIN person_role ON person_role.person_id = person.id GROUP BY person.id')
-        for [person_id, role_count] in result.fetchall():
+        for [person_id, role_count] in self.query('SELECT person.id, COUNT(person_role.role_id) FROM person LEFT JOIN person_role ON person_role.person_id = person.id GROUP BY person.id'):
             roleCounts[person_id] = role_count
         return roleCounts
 
     def slotsPerRoleCounts(self) -> dict:
         slotCounts = dict()
-        result = self.cursor.execute('SELECT role.id, COUNT(slot.id) FROM role LEFT JOIN slot ON slot.role_id = role.id GROUP BY role.id')
-        for [role_id, slot_count] in result.fetchall():
+        for [role_id, slot_count] in self.query('SELECT role.id, COUNT(slot.id) FROM role LEFT JOIN slot ON slot.role_id = role.id GROUP BY role.id'):
             slotCounts[role_id] = slot_count
         return slotCounts
 
     def onOneInXEvents(self) -> dict:
         onOneInXEvents = dict()
-        result = self.cursor.execute('SELECT * FROM person_role')
-        for [person_id, role_id, on_one_in_x_events] in result.fetchall():
+        for [person_id, role_id, on_one_in_x_events] in self.query('SELECT * FROM person_role'):
             if on_one_in_x_events != None:
                 onOneInXEvents[(person_id, role_id)] = on_one_in_x_events
         return onOneInXEvents
     
     def averageRoleCountsPerEvent(self) -> dict:
         averageRoleCountsPerEvent = dict()
-        result = self.cursor.execute('SELECT role_id, 1.0 * COUNT(id)/ COUNT(DISTINCT event_id) FROM slot GROUP BY role_id')
-        for [role_id, averageRoleCountPerEvent] in result.fetchall():
+        for [role_id, averageRoleCountPerEvent] in self.query('SELECT role_id, 1.0 * COUNT(id)/ COUNT(DISTINCT event_id) FROM slot GROUP BY role_id'):
             averageRoleCountsPerEvent[role_id] = averageRoleCountPerEvent
         return averageRoleCountsPerEvent
